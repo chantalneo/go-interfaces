7c9c11abdb7991b0c8a70aaa5513749666c6b1b9
Chantal Neo <Chantal@hotmail.sg>
2020-03-30 23:38:32 +0800

    Problems Without Interfaces

--- main.go
+++ main.go
@@ -1,7 +1,40 @@
 package main
 
-func main() {
+import "fmt"
 
+// Just making each of the bots a struct so I can create some functions to work with it. Thus, not associating any properties/fields
+type englishBot struct{}
+type spanishBot struct{}
+
+func main() {
+	eb := englishBot{}
+	sb := spanishBot{}
+
+	printGreeting(eb)
+	printGreeting(sb)
+}
+
+func printGreeting(eb englishBot) {
+	fmt.Println(eb.getGreeting())
+}
+
+func printGreeting(sb spanishBot) { // Go doesn't support overloading so the current project won't compile for now
+	fmt.Println(sb.getGreeting())
+}
+
+// func (eb englishBot) getGreeting() string { // Since we're not using eb, we can remove it entirely and just leave the receiver type right here
+func (englishBot) getGreeting() string {
+	// VERY custom logic for generating an English greeting
+	// Putting this imaginary requirement in here to make it clear that get greeting for both of these bots is going to contain some very different logic
+	// In other words, it's really unlikely that we're going to want to reuse the code inside of get greeting
+	return "Hi There!" // We're kind of just using a little bit of a contrived example, that's why we didn't create a field named greeting of string for the struct
+}
+
+func (spanishBot) getGreeting() string { // Omitted sb for the same reason for eb's
+	// VERY custom logic for generating a Spanish greeting
+	// Putting this imaginary requirement in here to make it clear that get greeting for both of these bots is going to contain some very different logic
+	// In other words, it's really unlikely that we're going to want to reuse the code inside of get greeting
+	return "Hola!" // We're kind of just using a little bit of a contrived example, that's why we didn't create a field named greeting of string for the struct
 }
 
 // Notes:
